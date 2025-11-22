695fd1d97ad6684cf3cef3b8b87f417ee486cc5a
James Windows Desktop <captaintriton167@gmail.com>
2025-11-22 21:05:56 +0000

    feat: realistic decay based on half-life

--- Instances/rad_source.gd
+++ Instances/rad_source.gd
@@ -1,6 +1,7 @@
 extends Marker2D
 
 @export var activity: float
+@onready var half_life: float = self.get_meta("half_life")
 
 func ln(x: float) -> float:
 	var e = exp(1)
@@ -9,12 +10,12 @@ func ln(x: float) -> float:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var half_life: float = self.get_meta("half_life")
 	var molar_mass: int = self.get_meta("element")
 	var mass: float = self.get_meta("mass")
 	
 	var nuclei = 6.022E23 * (mass / molar_mass)
 	
 	activity = nuclei * (ln(2) / half_life)
-	
-	print(ln(2))
+
+func _process(delta: float) -> void:
+	activity *= 0.5**(delta / half_life)
