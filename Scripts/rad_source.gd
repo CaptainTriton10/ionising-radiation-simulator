55210c430824cc58f0912e78c73ca388aa5633d2
CaptainTriton10 <captaintriton167@gmail.com>
2025-11-27 14:22:56 +0000

    feat: sprites for rad sources + detectors

--- Scripts/rad_source.gd
+++ Scripts/rad_source.gd
@@ -2,6 +2,7 @@ extends Marker2D
 
 @export var activity: float
 @onready var half_life: float = self.get_meta("half_life")
+@onready var mass: float = self.get_meta("mass")
 
 func ln(x: float) -> float:
 	var e = exp(1)
@@ -11,11 +12,10 @@ func ln(x: float) -> float:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var molar_mass: int = self.get_meta("element")
-	var mass: float = self.get_meta("mass")
 	
 	var nuclei = 6.022E23 * (mass / molar_mass)
 	
 	activity = nuclei * (ln(2) / half_life)
 
 func _process(delta: float) -> void:
-	activity *= 0.5**(delta / half_life)
+	mass *= 0.5**(delta / half_life)
--- Scripts/rad_source_label.gd
+++ Scripts/rad_source_label.gd
@@ -4,8 +4,13 @@ extends Label
 func _process(_delta: float) -> void:
 	var rad_source = $".."
 	
+	# Truncates 0s
 	var half_life = rad_source.get_meta("half_life")
-	half_life = int(half_life) if half_life > 1 else half_life # Truncate 0s
+	half_life = int(half_life) if half_life > 1 else half_life
 	
+	# Ditto
+	var mass = rad_source.mass
+	mass = int(mass) if mass > 5 else snapped(mass, 0.01)
+		
 	var label_text = "Mass: %sg\nAtomic Mass: %s\nHalf-life: %ss"
-	text = label_text % [rad_source.get_meta("mass"), rad_source.get_meta("element"), half_life]
+	text = label_text % [mass, rad_source.get_meta("element"), half_life]
