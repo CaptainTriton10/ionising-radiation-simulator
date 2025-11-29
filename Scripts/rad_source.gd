b85020c3cec698d2dd38e03987094e0bcb1f0450
CaptainTriton10 <captaintriton167@gmail.com>
2025-11-29 09:57:30 +0000

    feat: mass changes sprite size

--- Scripts/rad_source.gd
+++ Scripts/rad_source.gd
@@ -3,6 +3,7 @@ extends Marker2D
 @export var activity: float
 @onready var half_life: float = self.get_meta("half_life")
 @onready var mass: float = self.get_meta("mass")
+@onready var sprite: Sprite2D = $Sprite2D
 
 func ln(x: float) -> float:
 	var e = exp(1)
@@ -18,4 +19,7 @@ func _ready() -> void:
 	activity = nuclei * (ln(2) / half_life)
 
 func _process(delta: float) -> void:
+	# Decrease mass every tick (radioactive decay)
 	mass *= 0.5**(delta / half_life)
+	
+	sprite.scale = Vector2.ONE * minf(mass * 0.009, 1)
